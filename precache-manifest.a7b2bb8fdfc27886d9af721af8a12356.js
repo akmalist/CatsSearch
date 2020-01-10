570937de7c73d8219fe566c41427931e9e14ec7b
Aki <akmalqa101@gmail.com>
2020-01-10 13:23:58 -0800

    Updates

---
+++ precache-manifest.a7b2bb8fdfc27886d9af721af8a12356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7c3f51c02bad1615ff211c1314a144",
+    "url": "/CatsSearch/index.html"
+  },
+  {
+    "revision": "0031f999b08a7f481836",
+    "url": "/CatsSearch/static/css/main.45a761cb.chunk.css"
+  },
+  {
+    "revision": "6508fcff32f48a18f0fc",
+    "url": "/CatsSearch/static/js/2.fcd9bce9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/CatsSearch/static/js/2.fcd9bce9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0031f999b08a7f481836",
+    "url": "/CatsSearch/static/js/main.f769b1ee.chunk.js"
+  },
+  {
+    "revision": "5dea1599fca1b8c79ef8",
+    "url": "/CatsSearch/static/js/runtime-main.e887d3f9.js"
+  }
+]);
